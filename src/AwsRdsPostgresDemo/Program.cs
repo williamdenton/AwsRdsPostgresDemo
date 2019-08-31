2c00805965cbb1365d9159a7702d69464681723a
William Denton <william.denton@pushpay.com>
2019-08-31 14:53:04 +1200

    setup vscode

--- src/AwsRdsPostgresDemo/Program.cs
+++ src/AwsRdsPostgresDemo/Program.cs
@@ -2,11 +2,11 @@
 
 namespace WilliamDenton.AwsRdsPostgresDemo
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
 }
