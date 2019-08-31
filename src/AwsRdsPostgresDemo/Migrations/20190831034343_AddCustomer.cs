dec06ac6a19a41ba8143e5c4e5b94ecdbde0b5c0
William Denton <william.denton@pushpay.com>
2019-08-31 15:44:26 +1200

    add first migrations
    dotnet ef migrations add AddCustomer --context DemoMigratorDbContext

---
+++ src/AwsRdsPostgresDemo/Migrations/20190831034343_AddCustomer.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace WilliamDenton.AwsRdsPostgresDemo.Migrations
+{
+    public partial class AddCustomer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "customers",
+                columns: table => new
+                {
+                    id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(maxLength: 100, nullable: true),
+                    customer_code = table.Column<string>(maxLength: 10, nullable: true),
+                    created_on = table.Column<Instant>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_customers", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customers_customer_code",
+                table: "customers",
+                column: "customer_code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_customers_name",
+                table: "customers",
+                column: "name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "customers");
+        }
+    }
+}
