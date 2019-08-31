ee9f824500849e69a416abd02de09ac4603d2839
William Denton <william.denton@pushpay.com>
2019-08-31 14:48:16 +1200

    create new project
    dotnet new sln --name AwsRdsPostgresDemo
    mkdir src
    cd src
    dotnet new console --name AwsRdsPostgresDemo
    cd ..
    dotnet sln add src/AwsRdsPostgresDemo/AwsRdsPostgresDemo.csproj

---
+++ readme.md
@@ -0,0 +1,3 @@
+# AWS RDS Postgres Demo
+
+Dotnet Core 3.0 project demonstrating use of Npgsql and EfCore with AWS.
---
+++ src/AwsRdsPostgresDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AwsRdsPostgresDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
